6d5e18270df48a9c49f45526d3512e95ab1beee4
Michael <darmodymichael@gmail.com>
2019-04-02 15:32:50 +0100

    Added tests for bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,13 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
   it 'responds to release bike' do
     expect(subject).to respond_to :release_bike
+  end
+
+  it 'creates a new Bike object' do
+    bike = subject.release_bike
+    expect(bike). to be_working
   end
 end
