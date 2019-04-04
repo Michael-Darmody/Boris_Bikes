8f07c87c0fac3ede89eec67d707fc50c68398f7e
Michael <darmodymichael@gmail.com>
2019-04-04 15:42:26 +0100

    Step 12, exception handling

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,18 +1,28 @@
 require 'docking_station'
-require 'bike'
+
 
 describe DockingStation do
-  it 'responds to release bike' do
+
+  it 'responds to release_bike' do
     expect(subject).to respond_to :release_bike
   end
 
-  it 'creates a new Bike object if bike is working' do
-    bike = subject.release_bike
-    expect(bike). to be_working
+  # it 'releases a new Bike object when working' do
+    # bike = subject.release_bike
+    # expect(bike).to be_working
+  # end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
   end
 
-  it 'docks something' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to eq bike
+  describe '#release_bike' do
+    it 'throws an error if the dock is empty' do
+    expect { (subject.release_bike) }.to raise_error("No bikes are docked")
+    end
   end
 end
